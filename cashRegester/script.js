9d17a58e25a7626a2421ae28a5430b912764bb81
mehdi rezaei arshad <mehdirezaeiarshad@gmail.com>
2025-02-26 03:50:25 +0330

    finally . it's done . . .

--- cashRegester/script.js
+++ cashRegester/script.js
@@ -1,9 +1,10 @@
 const priceInput = document.getElementById("price-input");
 const buttonDiv = document.getElementById("buttons-div");
-const cashFromCustomer = document.getElementById("cash-from-customer");
+const cashFromCustomer = document.getElementById("cash");
 const purchaseForm = document.getElementById("purchase-form");
+const changeDue = document.getElementById("change-due")
 const allSpans = [...document.querySelectorAll("#cash-in-drawer-div > p > span")]
-let price = 1.87;
+let price = 3.26;
 priceInput.value = price;
 let cid = [
   ['PENNY', 1.01],
@@ -17,9 +18,9 @@ let cid = [
   ['ONE HUNDRED', 100]
 ];
 
-let changeGiven = {
+let tempCid = []
 
-}
+let changeGiven = {}
 
 const cidUpdater = () => {
  allSpans.forEach((span, index) => span.innerText = cid[index][1])
@@ -34,85 +35,122 @@ for (let i = 0; i < 9; i++) {
 }
 
 const checkForPennies = (input) => {
-    const valHere = 0.01 * 100;
+    const valHere = 0.01 * 1000;
     const cidTex = cid[0][0];
-    while (input - valHere >= price * 100 && cid[0][1] >= 0.01) {
+    cid[0][1] = cid[0][1] * 1000
+    while (input >= valHere && cid[0][1] >= 10) {
         input = input - valHere;
-        cid[0][1] -= 0.01;
+        cid[0][1] -= 10;
         if (changeGiven[cidTex]) {
-            changeGiven[cidTex] += 0.01;
+            changeGiven[cidTex] += 10;
         }
         else {
-            changeGiven[cidTex] = 0.01;
+            changeGiven[cidTex] = 10;
         }
-        if (!(input - valHere >= price * 100) || !(cid[0][1] >= 0.01)) {
-            console.log(input);
-            console.log(changeGiven);
-            console.log(cid);
-    }
-    } 
+        }
+        if (!(input >= valHere) || !(cid[0][1] >= 10)) {
+            if (changeGiven[cidTex]) {
+                changeGiven[cidTex] = changeGiven[cidTex] / 1000;
+              }
+            cid[0][1] = cid[0][1] / 1000;
+            const outOfCash = cid.every((el) => el[1] === 0)
+              
+            if (input === 0 && outOfCash) {
+                changeDue.innerText = "Status: CLOSED";
+                Object.entries(changeGiven).forEach((el) => {
+                changeDue.innerText += ` ${el[0]}: $${el[1]} `
+                cidUpdater()
+            });
+            }
+            else if (input === 0) {
+                changeDue.innerText = "Status: OPEN"
+                Object.entries(changeGiven).forEach((el) => {
+                changeDue.innerText += ` ${el[0]}: $${el[1]} `
+                cidUpdater()
+            });
+            }
+            else {
+            changeDue.innerText = "Status: INSUFFICIENT_FUNDS";
+            cid = tempCid;
+            cidUpdater()
+        }
+    
+    }  
 }
 
 const checkForNickels = (input) => {
-    const valHere = 0.05 * 100;
+    const valHere = 0.05 * 1000;
     const cidTex = cid[1][0];
-    while (input - valHere >= price * 100 && cid[1][1] >= 0.05) {
+    cid[1][1] = cid[1][1] * 1000;
+    while (input >= valHere && cid[1][1] >= 500) {
         input = input - valHere;
-        cid[1][1] -= 0.05;
+        cid[1][1] -= 500;
         if (changeGiven[cidTex]) {
-            changeGiven[cidTex] += 0.05;
+            changeGiven[cidTex] += 500;
         }
         else {
-            changeGiven[cidTex] = 0.05;
+            changeGiven[cidTex] = 500;
         }
-        if (!(input - valHere >= price * 100) || !(cid[1][1] >= 0.05)) {
+        }
+        if (!(input >= valHere) || !(cid[1][1] >= 500)) {
+            if (changeGiven[cidTex]) {
+                changeGiven[cidTex] = changeGiven[cidTex] / 1000;
+              }
+            cid[1][1] = cid[1][1] / 1000;
             checkForPennies(input)
-    }
     } 
 }
 
 const checkForDimes = (input) => {
-    const valHere = 0.1 * 100;
+    const valHere = 0.1 * 1000;
     const cidTex = cid[2][0];
-    while (input - valHere >= price * 100 && cid[2][1] >= 0.1) {
+    cid[2][1] = cid[2][1] * 1000;
+    while (input >= valHere && cid[2][1] >= 100) {
         input = input - valHere;
-        cid[2][1] -= 0.1;
+        cid[2][1] -= 100;
         if (changeGiven[cidTex]) {
-            changeGiven[cidTex] += 0.1;
-            changeGiven[cidTex] = Math.round(changeGiven[cidTex]);
+            changeGiven[cidTex] += 100;    
         }
         else {
-            changeGiven[cidTex] = 0.1;
-            changeGiven[cidTex] = Math.round(changeGiven[cidTex]);
+            changeGiven[cidTex] = 100;
         }
-        if (!(input - valHere >= price * 100) || !(cid[2][1] >= 0.1)) {
-            checkForNickels(input)
-    }
-    } 
+        }
+        if (!(input >= valHere) || !(cid[2][1] >= 100)) {
+            if (changeGiven[cidTex]) {
+                changeGiven[cidTex] = changeGiven[cidTex] / 1000;
+              }
+            cid[2][1] = cid[2][1] / 1000;
+            checkForNickels(input);
+    }  
 }
 
 const checkForQuarters = (input) => {
     const valHere = 0.25 * 100;
     const cidTex = cid[3][0];
-    while (input - valHere >= price * 100 && cid[3][1] >= 0.25) {
+    cid[3][1] = cid[3][1] * 100;
+    while (input >= valHere && cid[3][1] >= 25) {
         input = input - valHere;
-        cid[3][1] -= 0.25;
+        cid[3][1] -= 25;
         if (changeGiven[cidTex]) {
-            changeGiven[cidTex] += 0.25;
+            changeGiven[cidTex] += 25;
         }
         else {
-            changeGiven[cidTex] = 0.25;
+            changeGiven[cidTex] = 25;
         }
-        if (!(input - valHere >= price * 100) || !(cid[3][1] >= 0.25)) {
-            checkForDimes(input);
-    }
-    } 
+        }
+        if (!(input >= valHere) || !(cid[3][1] >= 25)) {
+            if (changeGiven[cidTex]) {
+              changeGiven[cidTex] = changeGiven[cidTex] / 100;
+            }
+           cid[3][1] = cid[3][1] / 100;
+           checkForDimes(input * 10)
+    }  
 }
 
 const checkForOnes = (input) => {
     const valHere = 1 * 100;
     const cidTex = cid[4][0];
-    while (input - valHere >= price * 100 && cid[4][1] >= 1) {
+    while (input >= valHere && cid[4][1] >= 1) {
         input = input - valHere;
         cid[4][1] -= 1;
         if (changeGiven[cidTex]) {
@@ -121,16 +159,16 @@ const checkForOnes = (input) => {
         else {
             changeGiven[cidTex] = 1;
         }
-        if (!(input - valHere >= price * 100) || !(cid[4][1] >= 1)) {
-            checkForQuarters(input)
-    }
+        }
+        if (!(input >= valHere) || !(cid[4][1] >= 1)) {
+            checkForQuarters(input);
     } 
 }
 
 const checkForFives = (input) => {
     const valHere = 5 * 100;
     const cidTex = cid[5][0];
-    while (input - valHere >= price * 100 && cid[5][1] >= 5) {
+    while (input >= valHere && cid[5][1] >= 5) {
         input = input - valHere;
         cid[5][1] -= 5;
         if (changeGiven[cidTex]) {
@@ -139,16 +177,16 @@ const checkForFives = (input) => {
         else {
             changeGiven[cidTex] = 5;
         }
-        if (!(input - valHere >= price * 100) || !(cid[5][1] >= 5)) {
-            checkForOnes(input)
-    }
-    } 
+        }
+        if (!(input >= valHere) || !(cid[5][1] >= 5)) {
+            checkForOnes(input);
+    }  
 }
 
 const checkForTens = (input) => {
     const valHere = 10 * 100;
     const cidTex = cid[6][0];
-    while (input - valHere >= price * 100 && cid[6][1] >= 10) {
+    while (input >= valHere && cid[6][1] >= 10) {
         input = input - valHere;
         cid[6][1] -= 10;
         if (changeGiven[cidTex]) {
@@ -157,16 +195,16 @@ const checkForTens = (input) => {
         else {
             changeGiven[cidTex] = 10;
         }
-        if (!(input - valHere >= price * 100) || !(cid[6][1] >= 10)) {
-            checkForFives(input)
+        }
+        if (!(input >= valHere) || !(cid[6][1] >= 10)) {
+            checkForFives(input);
     }
-    } 
 }
 
 const checkForTwenties = (input) => {
     const valHere = 20 * 100;
     const cidTex = cid[7][0];
-    while (input - valHere >= price * 100 && cid[7][1] >= 20) {
+    while (input >= valHere && cid[7][1] >= 20) {
         input = input - valHere;
         cid[7][1] -= 20;
         if (changeGiven[cidTex]) {
@@ -175,16 +213,16 @@ const checkForTwenties = (input) => {
         else {
             changeGiven[cidTex] = 20;
         }
-        if (!(input - valHere >= price * 100) || !(cid[7][1] >= 20)) {
-            checkForTens(input)
-    }
-    } 
+        }
+        if (!(input >= valHere) || !(cid[7][1] >= 20)) {
+            checkForTens(input);
+    }   
 }
 
 const checkForHundreds = (input) => {
     const valHere = 100 * 100;
     const cidTex = cid[8][0];
-    while (input - valHere >= price * 100 && cid[8][1] >= 100) {
+    while (input >= valHere && cid[8][1] >= 100) {
         input = input - valHere;
         cid[8][1] -= 100;
         if (changeGiven[cidTex]) {
@@ -193,10 +231,10 @@ const checkForHundreds = (input) => {
         else {
             changeGiven[cidTex] = 100;
         }
-        if (!(input - valHere >= price * 100) || !(cid[8][1] >= 100)) {
-            checkForTwenties(input)
-    }
-    } 
+        }
+        if (!(input >= valHere) || !(cid[8][1] >= 100)) {
+            checkForTwenties(input);          
+    }  
 }
 
 const checkInput = (input) => {
@@ -205,20 +243,22 @@ const checkInput = (input) => {
         return
     }
     else if (input === price) {
-        alert("No change due - customer paid with exact cash")
+        changeDue.innerText = "No change due - customer paid with exact cash";
         return
     }
     else {
-        checkForHundreds(input * 100)
+        changeGiven = {};
+        changeDue.innerText = "";
+        tempCid = cid;
+        checkForHundreds((input * 100) - (price * 100));
     }
 }
 
 purchaseForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    changeGiven = {};
     checkInput(Number(cashFromCustomer.value))
 })
 
 priceInput.addEventListener("onchange", () => {
-    price = Number(priceInput.value);
+    price = parseInt(priceInput.value);
 })
